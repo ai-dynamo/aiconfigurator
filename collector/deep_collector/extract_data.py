fc84191631b89d66da5ffb58c74aed6285c1d02a
AichenF <aichenf@nvidia.com>
2025-10-11 22:45:31 -0700

    add header

--- collector/deep_collector/extract_data.py
+++ collector/deep_collector/extract_data.py
@@ -1,3 +1,5 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
 #!/usr/bin/env python3
 """
 DeepEP cross-node test log analysis script
