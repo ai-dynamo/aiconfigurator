10d56df1095fb4cd1b11fe508812b836a5cb50a2
Jason Zhou <jasonzho@nvidia.com>
2025-10-19 17:11:06 -0700

    fix conv1d_update collector

--- collector/trtllm/collect_conv1d.py
+++ collector/trtllm/collect_conv1d.py
@@ -103,43 +103,48 @@ def get_conv1d_update_test_cases():
     
     Test parameters:
     - batch_size: batch size
-    - isl: sequence length
     - conv_kernel_size: size of the convolution kernel
     - conv_dim: dimension of the convolution
     - tp_size: attention tensor parallel size
+
+    Note: isl (sequence length) is not used for conv1d_update as it processes
+    individual tokens in incremental/streaming inference mode.
     """
     b_list = [1,2,4,8,16,32,64,128,256,512,1024,2048]
-    s_list = [2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072]
     tp_sizes = [1, 2, 4, 8]
     conv_dims = [1,2,4,8,16,32]
     kernel_sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]    
 
     test_cases = []
     for batch_size in b_list:
-        for isl in s_list:
-            for tp_size in tp_sizes:
-                for conv_dim in conv_dims:
-                    for kernel_size in kernel_sizes:
-                        test_cases.append([batch_size, isl, kernel_size, conv_dim, tp_size, 'conv1d_update_perf.txt'])
+        for tp_size in tp_sizes:
+            for conv_dim in conv_dims:
+                for kernel_size in kernel_sizes:
+                    test_cases.append([batch_size, kernel_size, conv_dim, tp_size, 'conv1d_update_perf.txt'])
 
     return test_cases
 
 
-def run_conv1d_update(batch_size, isl, conv_kernel_size, conv_dim, tp_size, perf_filename, device='cuda:0'):
+def run_conv1d_update(batch_size, conv_kernel_size, conv_dim, tp_size, perf_filename, device='cuda:0'):
     """
     Run Conv1DUpdate performance benchmarking.
     
     Args:
         batch_size: Batch size
-        isl: Sequence length
         conv_kernel_size: Size of the convolution kernel
         conv_dim: Dimension of the convolution
         tp_size: Attention tensor parallel size
         perf_filename: Output file for performance results
         device: CUDA device to use
+
+    Note: isl (sequence length) is not used as conv1d_update processes individual
+    tokens in incremental/streaming inference mode.
     """
     dtype = torch.bfloat16
-    mixed_qkv = torch.randn((batch_size * isl, conv_dim // tp_size), dtype=dtype).to(torch.device(device))
+    # Create input with shape (batch_size, dim)
+    mixed_qkv = torch.randn((batch_size, conv_dim // tp_size), dtype=dtype, device=device)
+    # Create conv_state with shape (batch_size, dim, kernel_size - 1)
+    conv_state = torch.randn((batch_size, conv_dim // tp_size, conv_kernel_size - 1), dtype=dtype, device=device)
     conv1d_weights = torch.randn((conv_dim // tp_size, conv_kernel_size), dtype=dtype, device=device)
 
     g = torch.cuda.CUDAGraph()
@@ -147,6 +152,7 @@ def run_conv1d_update(batch_size, isl, conv_kernel_size, conv_dim, tp_size, perf
         # TODO: measure optional arguments
         causal_conv1d_update(
             mixed_qkv,
+            conv_state,
             conv1d_weights,
         )
 
@@ -170,7 +176,6 @@ def run_conv1d_update(batch_size, isl, conv_kernel_size, conv_dim, tp_size, perf
     log_perf(
         item_list=[{ 
             'batch_size': batch_size,
-            'isl': isl,
             'conv_kernel_size': conv_kernel_size,
             'conv_dim': conv_dim,
             'tp_size': tp_size,
