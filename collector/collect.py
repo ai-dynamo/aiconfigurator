46ca6934248692fb3dddc9ceabee18add171be85
Jason Zhou <jasonzho@nvidia.com>
2025-10-20 14:28:40 -0700

    proper collect

--- collector/collect.py
+++ collector/collect.py
@@ -354,111 +354,111 @@ def collect_trtllm(num_processes: int, ops: List[str]=None):
     
     # Define collection modules - each test type as separate entry
     collections = [
-        # # GEMM collections
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'gemm_trt',
-        #     'module': 'trtllm.collect_gemm_trt',
-        #     'get_func': 'get_gemm_test_cases',
-        #     'run_func': 'run_gemm'
-        # },
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'gemm',
-        #     'module': 'trtllm.collect_gemm',
-        #     'get_func': 'get_gemm_test_cases',
-        #     'run_func': 'run_gemm'
-        # },
+        # GEMM collections
+        {
+            'name': 'trtllm',
+            'type': 'gemm_trt',
+            'module': 'trtllm.collect_gemm_trt',
+            'get_func': 'get_gemm_test_cases',
+            'run_func': 'run_gemm'
+        },
+        {
+            'name': 'trtllm',
+            'type': 'gemm',
+            'module': 'trtllm.collect_gemm',
+            'get_func': 'get_gemm_test_cases',
+            'run_func': 'run_gemm'
+        },
         
-        # # MLA collections
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'mla_context',
-        #     'module': 'trtllm.collect_mla',
-        #     'get_func': 'get_context_mla_test_cases',
-        #     'run_func': 'run_mla',
-        #     'version_handler': lambda v: 'trtllm.collect_mla_1_1rc2' if v.startswith('1.1')
-        #                                 else 'trtllm.collect_mla'
-        # },
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'mla_generation',
-        #     'module': 'trtllm.collect_mla',
-        #     'get_func': 'get_generation_mla_test_cases',
-        #     'run_func': 'run_mla',
-        #     'version_handler': lambda v: 'trtllm.collect_mla_1_1rc2' if v.startswith('1.1')
-        #                                 else 'trtllm.collect_mla'
-        # },
+        # MLA collections
+        {
+            'name': 'trtllm',
+            'type': 'mla_context',
+            'module': 'trtllm.collect_mla',
+            'get_func': 'get_context_mla_test_cases',
+            'run_func': 'run_mla',
+            'version_handler': lambda v: 'trtllm.collect_mla_1_1rc2' if v.startswith('1.1')
+                                        else 'trtllm.collect_mla'
+        },
+        {
+            'name': 'trtllm',
+            'type': 'mla_generation',
+            'module': 'trtllm.collect_mla',
+            'get_func': 'get_generation_mla_test_cases',
+            'run_func': 'run_mla',
+            'version_handler': lambda v: 'trtllm.collect_mla_1_1rc2' if v.startswith('1.1')
+                                        else 'trtllm.collect_mla'
+        },
         
-        # # Attention collections - separate entries for context and generation
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'attention_context',
-        #     'module': 'trtllm.collect_attn',
-        #     'get_func': 'get_context_attention_test_cases',
-        #     'run_func': 'run_attention_torch'
-        # },
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'attention_generation',
-        #     'module': 'trtllm.collect_attn',
-        #     'get_func': 'get_generation_attention_test_cases',
-        #     'run_func': 'run_attention_torch'
-        # },
+        # Attention collections - separate entries for context and generation
+        {
+            'name': 'trtllm',
+            'type': 'attention_context',
+            'module': 'trtllm.collect_attn',
+            'get_func': 'get_context_attention_test_cases',
+            'run_func': 'run_attention_torch'
+        },
+        {
+            'name': 'trtllm',
+            'type': 'attention_generation',
+            'module': 'trtllm.collect_attn',
+            'get_func': 'get_generation_attention_test_cases',
+            'run_func': 'run_attention_torch'
+        },
         
-        # # MLA BMM collections
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'mla_bmm_gen_pre',
-        #     'module': 'trtllm.collect_mla_bmm',
-        #     'get_func': 'get_mla_gen_pre_test_cases',
-        #     'run_func': 'run_mla_gen_pre'
-        # },
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'mla_bmm_gen_post',
-        #     'module': 'trtllm.collect_mla_bmm',
-        #     'get_func': 'get_mla_gen_post_test_cases',
-        #     'run_func': 'run_mla_gen_post'
-        # },
+        # MLA BMM collections
+        {
+            'name': 'trtllm',
+            'type': 'mla_bmm_gen_pre',
+            'module': 'trtllm.collect_mla_bmm',
+            'get_func': 'get_mla_gen_pre_test_cases',
+            'run_func': 'run_mla_gen_pre'
+        },
+        {
+            'name': 'trtllm',
+            'type': 'mla_bmm_gen_post',
+            'module': 'trtllm.collect_mla_bmm',
+            'get_func': 'get_mla_gen_post_test_cases',
+            'run_func': 'run_mla_gen_post'
+        },
         
-        # # MOE collection (with version handling)
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'moe',
-        #     'module': None,  # Will be determined based on version
-        #     'get_func': 'get_moe_test_cases',
-        #     'run_func': 'run_moe_torch',
-        #     'version_handler': lambda v: 'trtllm.collect_moe_pre_0_20' if v.startswith('0.20.0') 
-        #                               else 'trtllm.collect_moe_pre_1_0' if v.startswith(('0.21.0', '1.0.0'))
-        #                               else 'trtllm.collect_moe' if v.startswith(('1.1.0'))
-        #                               else None
-        # },
+        # MOE collection (with version handling)
+        {
+            'name': 'trtllm',
+            'type': 'moe',
+            'module': None,  # Will be determined based on version
+            'get_func': 'get_moe_test_cases',
+            'run_func': 'run_moe_torch',
+            'version_handler': lambda v: 'trtllm.collect_moe_pre_0_20' if v.startswith('0.20.0') 
+                                      else 'trtllm.collect_moe_pre_1_0' if v.startswith(('0.21.0', '1.0.0'))
+                                      else 'trtllm.collect_moe' if v.startswith(('1.1.0'))
+                                      else None
+        },
 
         # CONV 1D collections
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'conv1d_fn',
-        #     'module': 'trtllm.collect_conv1d',
-        #     'get_func': 'get_conv1d_fn_test_cases',
-        #     'run_func': 'run_conv1d_fn'
-        # },
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'conv1d_update',
-        #     'module': 'trtllm.collect_conv1d',
-        #     'get_func': 'get_conv1d_update_test_cases',
-        #     'run_func': 'run_conv1d_update'
-        # },
+        {
+            'name': 'trtllm',
+            'type': 'conv1d_fn',
+            'module': 'trtllm.collect_conv1d',
+            'get_func': 'get_conv1d_fn_test_cases',
+            'run_func': 'run_conv1d_fn'
+        },
+        {
+            'name': 'trtllm',
+            'type': 'conv1d_update',
+            'module': 'trtllm.collect_conv1d',
+            'get_func': 'get_conv1d_update_test_cases',
+            'run_func': 'run_conv1d_update'
+        },
 
         # Gated Delta Rule collections
-        # {
-        #     'name': 'trtllm',
-        #     'type': 'chunk_gated_delta_rule',
-        #     'module': 'trtllm.collect_gated_delta_rule',
-        #     'get_func': 'get_chunk_gated_delta_rule_test_cases',
-        #     'run_func': 'run_chunk_gated_delta_rule'
-        # },
+        {
+            'name': 'trtllm',
+            'type': 'chunk_gated_delta_rule',
+            'module': 'trtllm.collect_gated_delta_rule',
+            'get_func': 'get_chunk_gated_delta_rule_test_cases',
+            'run_func': 'run_chunk_gated_delta_rule'
+        },
         {
             'name': 'trtllm',
             'type': 'gated_delta_rule_update',
@@ -561,7 +561,8 @@ def main():
     parser.add_argument('--ops', nargs='*', type=str, choices=['gemm_trt', 'gemm', 'mla_context', 
                                                                'mla_generation', 'attention_context',
                                                                'attention_generation', 'mla_bmm_gen_pre',
-                                                               'mla_bmm_gen_post', 'moe'],
+                                                               'mla_bmm_gen_post', 'moe', 'conv1d_fn', 'conv1d_update',
+                                                               'chunk_gated_delta_rule', 'gated_delta_rule_update'],
                         help='Run only specified collection items. Leave empty to run all.',
                         default=None)
     args = parser.parse_args()
