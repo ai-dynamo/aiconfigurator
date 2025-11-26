167215219854f7e171420c626d73da1d1cdf0aa0
Xue Huang <xueh@nvidia.com>
2025-11-26 02:57:43 +0000

    update
    Signed-off-by: Xue Huang <xueh@nvidia.com>

--- src/aiconfigurator/webapp/main.py
+++ src/aiconfigurator/webapp/main.py
@@ -77,7 +77,12 @@ def main(args):
                         Version {aiconfigurator.__version__}
                     </p>
                     <p style="font-size: 12px; margin-top: -10px; color: #666;">
-                        Use of this service is for test and evaluation purposes only. Results are estimates and may be inaccurate. The AI Configurator software available at <a href="https://github.com/ai-dynamo/aiconfigurator/" target="_blank">https://github.com/ai-dynamo/aiconfigurator/</a> is governed by the Apache 2.0 License.
+                        Use of this service is for test and evaluation purposes only.
+                        Results are estimates and may be inaccurate.
+                        The AI Configurator software available at
+                        <a href="https://github.com/ai-dynamo/aiconfigurator/" target="_blank">
+                        https://github.com/ai-dynamo/aiconfigurator/</a>
+                        is governed by the Apache 2.0 License.
                     </p>
                 </div>
                 """
