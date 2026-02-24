67a0e9551576e0018d69278c68c878c99f704596
Ilya Sherstyuk <isherstyuk@nvidia.com>
2026-02-24 12:01:11 -0800

    remove accidental addition
    Signed-off-by: Ilya Sherstyuk <isherstyuk@nvidia.com>

--- src/aiconfigurator/webapp/main.py
+++ src/aiconfigurator/webapp/main.py
@@ -8,9 +8,6 @@ from collections import defaultdict
 import aiconfigurator
 from aiconfigurator.sdk import perf_database
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger(__name__)
-
 
 def is_editable_install() -> bool:
     """
