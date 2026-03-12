25f134a453d2817d42c411471b3bfdff3e818de6
yimingl (generated by with_the_same_user script) <yimingl@gb-nvl-082-compute05.nvidia.com>
2026-03-12 18:24:35 +0000

    bug fix
    Signed-off-by: yimingl (generated by with_the_same_user script) <yimingl@gb-nvl-082-compute05.nvidia.com>

--- collector/trtllm/collect_moe_v3.py
+++ collector/trtllm/collect_moe_v3.py
@@ -150,6 +150,7 @@ def get_moe_test_cases():
                 "fp8": 8,
                 "fp8_block": 8,
                 "w4a16_mxfp4": 4,
+                "w4a8_mxfp4_mxfp8": 4,
                 "w4afp8": 4,
                 "nvfp4": 4,
             }[moe_type]
