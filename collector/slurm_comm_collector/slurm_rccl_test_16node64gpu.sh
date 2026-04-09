7ff7e15bbdf20a86565f221c1a3a21323c2fd0c5
Theresa Shan <theresa.shan@amd.com>
2026-04-09 09:13:43 +0000

    add source code to generate rccl test result
    Signed-off-by: Theresa Shan <theresa.shan@amd.com>

---
+++ collector/slurm_comm_collector/slurm_rccl_test_16node64gpu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#SBATCH -N 16
+#SBATCH --gpus 64
+#SBATCH --ntasks-per-node=4
+#SBATCH -o log_rccl/16node64gpu.out
+#SBATCH -e error_rccl/16node64gpu.err
+#SBATCH -J 16node64gpu
+
+export NCCL_DEBUG=ERROR
+export NCCL_SOCKET_IFNAME=enp81s0f1
+export NCCL_NET_GDR_LEVEL=SYS
+export NCCL_IB_HCA=ionic_0,ionic_1,ionic_2,ionic_3,ionic_4,ionic_5,ionic_6,ionic_7
+export UCX_NET_DEVICES=benic1p1:1,benic2p1:1,benic3p1:1,benic4p1:1,benic5p1:1,benic6p1:1,benic7p1:1,benic8p1:1
+# export NCCL_ALGO=ring
+
+srun -l \
+    --ntasks 64 --ntasks-per-node 4 \
+    --container-image=lmsysorg/sglang:v0.5.9-rocm720-mi35x \
+    --container-mounts=/dev:/dev,${HOME}:${HOME},/apps/theresa:/workspace \
+    --container-workdir=/workspace \
+    --export=ALL \
+    --mpi=pmix bash -c "/workspace/rccl-tests/build/all_reduce_perf -b 256 -e 8g -d half -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/all_reduce_perf -b 256 -e 8g -d int8 -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/alltoall_perf -b 256 -e 8g -d half -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/alltoall_perf -b 256 -e 8g -d int8 -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/reduce_scatter_perf -b 256 -e 8g -d half -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/reduce_scatter_perf -b 256 -e 8g -d int8 -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/all_gather_perf -b 256 -e 8g -d half -f2 -g 1 -w 40 -a 1 -n 60 -c 0; \
+                        /workspace/rccl-tests/build/all_gather_perf -b 256 -e 8g -d int8 -f2 -g 1 -w 40 -a 1 -n 60 -c 0"
